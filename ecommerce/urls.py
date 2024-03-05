2fcc19506568389c55f70a0592d5956b23da79df
Maxim Gordilov <m.gordilov@yahoo.com>
2024-03-05 21:21:39 +0000

    Added users app and some models

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ecommerce_app.urls')),
+    path('', include('users.urls')),
 ]
--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.utils import timezone
+from users.models import UserProfile, Business
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    business = models.ForeignKey(Business, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[str(self.id)])
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    product = models.ManyToManyField(Product)
+
+class WishList(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    product = models.ManyToManyField(Product)
