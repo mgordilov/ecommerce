47ef66f38ac36930ed5d5896737210d70e65ec11
Maxim Gordilov <m.gordilov@yahoo.com>
2024-03-07 00:23:55 +0000

    Improved business creation

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Business(models.Model):
@@ -25,3 +27,10 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     business = models.ForeignKey(Business, on_delete=models.CASCADE, blank=True, null=True)
     stripe_id = models.CharField(max_length=50, blank=True, null=True)
+
+@receiver(post_save, sender=Business)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        user = instance.owner
+        if not UserProfile.objects.filter(user=user).exists():
+            UserProfile.objects.create(user=user, business=instance)
