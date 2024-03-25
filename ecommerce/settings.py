31a56dc7da3bb73ee034f8792753d5febc15acb0
Maxim Gordilov <m.gordilov@yahoo.com>
2024-03-25 22:34:39 +0000

    Password reset feature

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -60,6 +60,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 ROOT_URLCONF = 'ecommerce.urls'
 
 TEMPLATES = [
