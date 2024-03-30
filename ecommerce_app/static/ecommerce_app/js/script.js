1c1c44e5221e6dc918fb43678c6a7c0cb5b53f22
Maxim Gordilov <m.gordilov@yahoo.com>
2024-03-30 02:08:45 +0000

    Styling

--- ecommerce_app/static/ecommerce_app/js/script.js
+++ ecommerce_app/static/ecommerce_app/js/script.js
@@ -6,3 +6,30 @@ function activeTab() {
         activeLink.classList.add('active');
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    function toggleButtonVisibility(productsElement, buttonElement) {
+        productsElement.addEventListener('mouseover', () => {
+            buttonElement.classList.add('showButton');
+        });
+        productsElement.addEventListener('mouseout', () => {
+            buttonElement.classList.remove('showButton');
+        });
+    }
+
+    const productsIntro = document.getElementById('productsIntro');
+    const productsIntroButton = document.getElementById('productsIntroButton');
+    toggleButtonVisibility(productsIntro, productsIntroButton);
+
+    const womenProducts = document.getElementById('womenProducts');
+    const womenButton = document.getElementById('womenButton');
+    toggleButtonVisibility(womenProducts, womenButton);
+
+    const menProducts = document.getElementById('menProducts');
+    const menButton = document.getElementById('menButton');
+    toggleButtonVisibility(menProducts, menButton);
+
+    const kidsProducts = document.getElementById('kidsProducts');
+    const kidsButton = document.getElementById('kidsButton');
+    toggleButtonVisibility(kidsProducts, kidsButton);
+});
